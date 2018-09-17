5c50a282cbf0fa7a0112bd3a51dbdffea52d98eb
fawda123 <mbafs2012@gmail.com>
2018-09-16 21:28:21 -0700

    added bs target table

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -4,10 +4,17 @@ library(tidyverse)
 thrsdat <- read.csv('raw/BIthresholds.csv', stringsAsFactors = F)
 
 # site bs values and bi values
-bsbidat <- read.csv('raw/mydf.c3.csv', stringsAsFactors = F)
+# filter only selected sites
+bsbidat <- read.csv('raw/mydf.c3.csv', stringsAsFactors = F) %>% 
+  filter(SelectedSample %in% 'Selected') %>% 
+  select(-SelectedSample, -New_Lat, -New_Long, -PSA6c, -PCT_MAP) %>% 
+  gather('bsvar', 'bsval', Nitrogen_Total_mgPerL:Ash_Free_Dry_Mass_mgPercm2) %>% 
+  gather('bivar', 'bival', CSCI:ASCI_H)
 
 # site counts of passing/failing by each target, with probabilities
-tllydat <- read.csv('raw/tab.threshold.rr.summary.csv', stringsAsFactors = F)
+# filter those where RR.l95.cal and RR.l95.val > 1
+tllydat <- read.csv('raw/tab.threshold.rr.summary.csv', stringsAsFactors = F) %>% 
+  filter(RR.l95.cal > 1 & RR.l95.val > 1)
 
 save(thrsdat, file = 'data/thrsdat.RData', compress = 'xz')
 save(bsbidat, file = 'data/bsbidat.RData', compress = 'xz')
